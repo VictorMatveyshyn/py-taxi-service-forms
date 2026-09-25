acc1b214399e5a50365e3762932d4e7061e1d737
Victor Matveyshin <vic_m@ukr.net>
2026-09-25 22:29:29 +0300

    taxi-forms point 5.3

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Manufacturer
+from .models import Manufacturer, Car
 
 
 class ManufacturerForm(forms.ModelForm):
     class Meta:
         model = Manufacturer
+        fields = "__all__"
+
+
+class CarForm(forms.ModelForm):
+    class Meta:
+        model = Car
         fields = "__all__"
